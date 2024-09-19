088ea467cb46c7c710048099a30313b438c6d96c
JairGarcia29 <jamesjair29@gmail.com>
2024-09-19 09:23:57 -0500

    first commit

---
+++ P1.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double sbruto, ventas, salario=0, total, años, bono=0, dscto=0, sueldoto;
+        string nom;
+        Console.WriteLine("Inserte su nombre");
+        nom=Console.ReadLine();
+        Console.WriteLine("Inserte su salario bruto");
+        sbruto=double.Parse(Console.ReadLine());
+        Console.WriteLine("Inserte su numero de ventas");
+        ventas=double.Parse(Console.ReadLine());
+        Console.WriteLine("Inserte su numero de años en la empresa");
+        años=double.Parse(Console.ReadLine());
+        if (ventas <= 600) 
+        {
+            bono = ventas * 0.05;
+        }else
+        {
+            if (ventas >= 600 || ventas <=3000)
+            {
+                bono = ventas * 0.07;
+            }
+            else
+            {
+                if (ventas > 3000)
+                {
+                    bono = (ventas * 0.06) + 300;
+                }
+            }
+        }
+        total = sbruto + bono;
+        if (años <= 3)
+        {
+            dscto = total * 0.065;
+        } else if (años >= 4)
+        {
+            dscto = total * 0.045;
+        }
+        sbruto= total - dscto;
+        Console.WriteLine($"Nombre:{nom}");
+        Console.WriteLine($"Su salario bruto es: {sbruto}");
+        Console.WriteLine($"Su numero de ventas es:{ventas} ");
+        Console.WriteLine($"Numero de años en la empresa: {años}");
+    }
+}
---
+++ P2.cs
@@ -0,0 +1,37 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int p1, verde = 0, azul = 0, rojo = 0;
+        String o = "v";
+        Console.WriteLine("¿Continuar con el programa? (s/n)");
+        o = Console.ReadLine().ToLower();
+        while (o == "s")
+        {
+            Console.WriteLine("Ingrese el ultimo digito de la placa");
+            p1= int.Parse(Console.ReadLine());
+            if (p1 >= 0 & p1 <= 3)
+            {
+                verde++;
+            }
+            else 
+            
+            if (p1 >= 4 & p1 <= 6)
+            {
+                azul++;
+            }
+            else 
+            if (p1 >= 7 & p1 <= 9)
+            {
+                rojo++;
+            }
+            Console.WriteLine("Continuar con el programa? (v/f)");
+             o = Console.ReadLine().ToLower();
+        }       
+        Console.WriteLine($"Verde: {verde}");
+        Console.WriteLine($"Azul: {azul}");
+        Console.WriteLine($"Rojo: {rojo}");
+        Console.WriteLine($"Total de autos: {azul + verde + rojo}");
+    }
+}
